863ec6efb82e485893a12e18bb4feeec9157ad4e
Omri Bernstein <ob08@hampshire.edu>
2013-11-06 12:56:28 -0500

    Adds default-quantum-system and new-quantum-system

--- src/clj/qgame/core.clj
+++ src/clj/qgame/core.clj
@@ -33,3 +33,27 @@
 (defn -main [& args]
   (jetty/run-jetty app {:port 3000}))
 
+(defn expt
+  "Lifted from clojure.math.numeric-tower"
+  [base pow]
+  (loop [n pow y (num 1) z base]
+    (let [t (even? n) n (quot n 2)]
+      (cond
+        t (recur n y (* z z))
+        (zero? n) (* z y)
+        :else (recur n (* z y) (* z z))))))
+
+(def default-quantum-system
+  {:amplitudes []
+   :prior-probability 1
+   :oracle-count 0
+   :measurement-history ()
+   :instruction-history ()
+   :program ()})
+
+(defn new-quantum-system
+  [num-qubits]
+  (let [num-amplitudes (expt 2 num-qubits)]
+    (assoc default-quantum-system
+           :amplitudes (into [1] (repeat (dec num-amplitudes) 0)))))
+
