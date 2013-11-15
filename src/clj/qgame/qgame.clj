c49086c211d5773651bf757be3af91d6eaeb87b3
Omri Bernstein <ob08@hampshire.edu>
2013-11-14 22:57:07 -0500

    Will this commit work?

--- src/clj/qgame/qgame.clj
+++ src/clj/qgame/qgame.clj
@@ -2,17 +2,7 @@
   (:refer-clojure :exclude [* - + == /])
   (:use clojure.core.matrix)
   (:use clojure.core.matrix.operators)
-  (:use clojure.math.numeric-tower))
-
-#_(defn expt
-  "Lifted from clojure.math.numeric-tower"
-  [base pow]
-  (loop [n pow y (num 1) z base]
-    (let [t (even? n) n (quot n 2)]
-      (cond
-        t (recur n y (* z z))
-        (zero? n) (* z y)
-        :else (recur n (* z y) (* z z))))))
+  (:use [clojure.math.numeric-tower :only [expt]]))
 
 (def default-quantum-system
   {:amplitudes []
@@ -25,5 +15,7 @@
 (defn new-quantum-system
   [num-qubits]
   (let [num-amplitudes (expt 2 num-qubits)]
-    (assoc default-quantum-system
-           :amplitudes (into [1] (repeat (dec num-amplitudes) 0)))))
+    (->> 0
+      (repeat (dec num-amplitudes))
+      (into [1])
+      (assoc default-quantum-system :amplitudes))))
---
+++ target/cljsbuild-compiler-0/cljs/core.js
@@ -0,0 +1,355 @@
+goog.provide('cljs.core');
+goog.require('goog.array');
+goog.require('goog.array');
+goog.require('goog.object');
+goog.require('goog.object');
+goog.require('goog.string.StringBuffer');
+goog.require('goog.string.StringBuffer');
+goog.require('goog.string');
+goog.require('goog.string');
+cljs.core._STAR_unchecked_if_STAR_ = false;
+/**
+* Each runtime environment provides a diffenent way to print output.
+* Whatever function *print-fn* is bound to will be passed any
+* Strings which should be printed.
+*/
+cljs.core._STAR_print_fn_STAR_ = (function _STAR_print_fn_STAR_(_){throw (new Error("No *print-fn* fn set for evaluation environment"));
+});
+/**
+* Set *print-fn* to f.
+*/
+cljs.core.set_print_fn_BANG_ = (function set_print_fn_BANG_(f){return cljs.core._STAR_print_fn_STAR_ = f;
+});
+cljs.core._STAR_flush_on_newline_STAR_ = true;
+cljs.core._STAR_print_readably_STAR_ = true;
+cljs.core._STAR_print_meta_STAR_ = false;
+cljs.core._STAR_print_dup_STAR_ = false;
+cljs.core.pr_opts = (function pr_opts(){return cljs.core.PersistentArrayMap.fromArray([new cljs.core.Keyword(null,"flush-on-newline","flush-on-newline",4338025857),cljs.core._STAR_flush_on_newline_STAR_,new cljs.core.Keyword(null,"readably","readably",4441712502),cljs.core._STAR_print_readably_STAR_,new cljs.core.Keyword(null,"meta","meta",1017252215),cljs.core._STAR_print_meta_STAR_,new cljs.core.Keyword(null,"dup","dup",1014004081),cljs.core._STAR_print_dup_STAR_], true);
+});
+/**
+* Internal - do not use!
+*/
+cljs.core.truth_ = (function truth_(x){return (x != null && x !== false);
+});
+cljs.core.not_native = null;
+/**
+* Tests if 2 arguments are the same object
+*/
+cljs.core.identical_QMARK_ = (function identical_QMARK_(x,y){return (x === y);
+});
+/**
+* Returns true if x is nil, false otherwise.
+*/
+cljs.core.nil_QMARK_ = (function nil_QMARK_(x){return (x == null);
+});
+cljs.core.array_QMARK_ = (function array_QMARK_(x){return x instanceof Array;
+});
+cljs.core.number_QMARK_ = (function number_QMARK_(n){return typeof n === 'number';
+});
+/**
+* Returns true if x is logical false, false otherwise.
+*/
+cljs.core.not = (function not(x){if(cljs.core.truth_(x))
+{return false;
+} else
+{return true;
+}
+});
+cljs.core.string_QMARK_ = (function string_QMARK_(x){return goog.isString(x);
+});
+/**
+* Internal - do not use!
+*/
+cljs.core.type_satisfies_ = (function type_satisfies_(p,x){var x__$1 = (((x == null))?null:x);if((p[goog.typeOf(x__$1)]))
+{return true;
+} else
+{if((p["_"]))
+{return true;
+} else
+{if(new cljs.core.Keyword(null,"else","else",1017020587))
+{return false;
+} else
+{return null;
+}
+}
+}
+});
+cljs.core.is_proto_ = (function is_proto_(x){return (x.constructor.prototype === x);
+});
+/**
+* When compiled for a command-line target, whatever
+* function *main-fn* is set to will be called with the command-line
+* argv as arguments
+*/
+cljs.core._STAR_main_cli_fn_STAR_ = null;
+cljs.core.type = (function type(x){if((x == null))
+{return null;
+} else
+{return x.constructor;
+}
+});
+cljs.core.missing_protocol = (function missing_protocol(proto,obj){var ty = cljs.core.type.call(null,obj);var ty__$1 = (cljs.core.truth_((function (){var and__3941__auto__ = ty;if(cljs.core.truth_(and__3941__auto__))
+{return ty.cljs$lang$type;
+} else
+{return and__3941__auto__;
+}
+})())?ty.cljs$lang$ctorStr:goog.typeOf(obj));return (new Error(["No protocol method ",proto," defined for type ",ty__$1,": ",obj].join("")));
+});
+cljs.core.type__GT_str = (function type__GT_str(ty){var temp__4090__auto__ = ty.cljs$lang$ctorStr;if(cljs.core.truth_(temp__4090__auto__))
+{var s = temp__4090__auto__;return s;
+} else
+{return [cljs.core.str(ty)].join('');
+}
+});
+/**
+* Returns a javascript array, cloned from the passed in array
+*/
+cljs.core.aclone = (function aclone(array_like){return array_like.slice();
+});
+/**
+* Creates a new javascript array.
+* @param {...*} var_args
+*/
+cljs.core.array = (function array(var_args){return Array.prototype.slice.call(arguments);
+});
+cljs.core.make_array = (function() {
+var make_array = null;
+var make_array__1 = (function (size){return (new Array(size));
+});
+var make_array__2 = (function (type,size){return make_array.call(null,size);
+});
+make_array = function(type,size){
+switch(arguments.length){
+case 1:
+return make_array__1.call(this,type);
+case 2:
+return make_array__2.call(this,type,size);
+}
+throw(new Error('Invalid arity: ' + arguments.length));
+};
+make_array.cljs$core$IFn$_invoke$arity$1 = make_array__1;
+make_array.cljs$core$IFn$_invoke$arity$2 = make_array__2;
+return make_array;
+})()
+;
+/**
+* Returns the value at the index.
+* @param {...*} var_args
+*/
+cljs.core.aget = (function() {
+var aget = null;
+var aget__2 = (function (array,i){return (array[i]);
+});
+var aget__3 = (function() { 
+var G__4851__delegate = function (array,i,idxs){return cljs.core.apply.call(null,aget,aget.call(null,array,i),idxs);
+};
+var G__4851 = function (array,i,var_args){
+var idxs = null;if (arguments.length > 2) {
+  idxs = cljs.core.array_seq(Array.prototype.slice.call(arguments, 2),0);} 
+return G__4851__delegate.call(this,array,i,idxs);};
+G__4851.cljs$lang$maxFixedArity = 2;
+G__4851.cljs$lang$applyTo = (function (arglist__4852){
+var array = cljs.core.first(arglist__4852);
+arglist__4852 = cljs.core.next(arglist__4852);
+var i = cljs.core.first(arglist__4852);
+var idxs = cljs.core.rest(arglist__4852);
+return G__4851__delegate(array,i,idxs);
+});
+G__4851.cljs$core$IFn$_invoke$arity$variadic = G__4851__delegate;
+return G__4851;
+})()
+;
+aget = function(array,i,var_args){
+var idxs = var_args;
+switch(arguments.length){
+case 2:
+return aget__2.call(this,array,i);
+default:
+return aget__3.cljs$core$IFn$_invoke$arity$variadic(array,i, cljs.core.array_seq(arguments, 2));
+}
+throw(new Error('Invalid arity: ' + arguments.length));
+};
+aget.cljs$lang$maxFixedArity = 2;
+aget.cljs$lang$applyTo = aget__3.cljs$lang$applyTo;
+aget.cljs$core$IFn$_invoke$arity$2 = aget__2;
+aget.cljs$core$IFn$_invoke$arity$variadic = aget__3.cljs$core$IFn$_invoke$arity$variadic;
+return aget;
+})()
+;
+/**
+* Sets the value at the index.
+* @param {...*} var_args
+*/
+cljs.core.aset = (function() {
+var aset = null;
+var aset__3 = (function (array,i,val){return (array[i] = val);
+});
+var aset__4 = (function() { 
+var G__4853__delegate = function (array,idx,idx2,idxv){return cljs.core.apply.call(null,aset,(array[idx]),idx2,idxv);
+};
+var G__4853 = function (array,idx,idx2,var_args){
+var idxv = null;if (arguments.length > 3) {
+  idxv = cljs.core.array_seq(Array.prototype.slice.call(arguments, 3),0);} 
+return G__4853__delegate.call(this,array,idx,idx2,idxv);};
+G__4853.cljs$lang$maxFixedArity = 3;
+G__4853.cljs$lang$applyTo = (function (arglist__4854){
+var array = cljs.core.first(arglist__4854);
+arglist__4854 = cljs.core.next(arglist__4854);
+var idx = cljs.core.first(arglist__4854);
+arglist__4854 = cljs.core.next(arglist__4854);
+var idx2 = cljs.core.first(arglist__4854);
+var idxv = cljs.core.rest(arglist__4854);
+return G__4853__delegate(array,idx,idx2,idxv);
+});
+G__4853.cljs$core$IFn$_invoke$arity$variadic = G__4853__delegate;
+return G__4853;
+})()
+;
+aset = function(array,idx,idx2,var_args){
+var idxv = var_args;
+switch(arguments.length){
+case 3:
+return aset__3.call(this,array,idx,idx2);
+default:
+return aset__4.cljs$core$IFn$_invoke$arity$variadic(array,idx,idx2, cljs.core.array_seq(arguments, 3));
+}
+throw(new Error('Invalid arity: ' + arguments.length));
+};
+aset.cljs$lang$maxFixedArity = 3;
+aset.cljs$lang$applyTo = aset__4.cljs$lang$applyTo;
+aset.cljs$core$IFn$_invoke$arity$3 = aset__3;
+aset.cljs$core$IFn$_invoke$arity$variadic = aset__4.cljs$core$IFn$_invoke$arity$variadic;
+return aset;
+})()
+;
+/**
+* Returns the length of the array. Works on arrays of all types.
+*/
+cljs.core.alength = (function alength(array){return array.length;
+});
+cljs.core.into_array = (function() {
+var into_array = null;
+var into_array__1 = (function (aseq){return into_array.call(null,null,aseq);
+});
+var into_array__2 = (function (type,aseq){return cljs.core.reduce.call(null,(function (a,x){a.push(x);
+return a;
+}),[],aseq);
+});
+into_array = function(type,aseq){
+switch(arguments.length){
+case 1:
+return into_array__1.call(this,type);
+case 2:
+return into_array__2.call(this,type,aseq);
+}
+throw(new Error('Invalid arity: ' + arguments.length));
+};
+into_array.cljs$core$IFn$_invoke$arity$1 = into_array__1;
+into_array.cljs$core$IFn$_invoke$arity$2 = into_array__2;
+return into_array;
+})()
+;
+cljs.core.Fn = {};
+cljs.core.IFn = {};
+cljs.core._invoke = (function() {
+var _invoke = null;
+var _invoke__1 = (function (this$){if((function (){var and__3941__auto__ = this$;if(and__3941__auto__)
+{return this$.cljs$core$IFn$_invoke$arity$1;
+} else
+{return and__3941__auto__;
+}
+})())
+{return this$.cljs$core$IFn$_invoke$arity$1(this$);
+} else
+{var x__3502__auto__ = (((this$ == null))?null:this$);return (function (){var or__3943__auto__ = (cljs.core._invoke[goog.typeOf(x__3502__auto__)]);if(or__3943__auto__)
+{return or__3943__auto__;
+} else
+{var or__3943__auto____$1 = (cljs.core._invoke["_"]);if(or__3943__auto____$1)
+{return or__3943__auto____$1;
+} else
+{throw cljs.core.missing_protocol.call(null,"IFn.-invoke",this$);
+}
+}
+})().call(null,this$);
+}
+});
+var _invoke__2 = (function (this$,a){if((function (){var and__3941__auto__ = this$;if(and__3941__auto__)
+{return this$.cljs$core$IFn$_invoke$arity$2;
+} else
+{return and__3941__auto__;
+}
+})())
+{return this$.cljs$core$IFn$_invoke$arity$2(this$,a);
+} else
+{var x__3502__auto__ = (((this$ == null))?null:this$);return (function (){var or__3943__auto__ = (cljs.core._invoke[goog.typeOf(x__3502__auto__)]);if(or__3943__auto__)
+{return or__3943__auto__;
+} else
+{var or__3943__auto____$1 = (cljs.core._invoke["_"]);if(or__3943__auto____$1)
+{return or__3943__auto____$1;
+} else
+{throw cljs.core.missing_protocol.call(null,"IFn.-invoke",this$);
+}
+}
+})().call(null,this$,a);
+}
+});
+var _invoke__3 = (function (this$,a,b){if((function (){var and__3941__auto__ = this$;if(and__3941__auto__)
+{return this$.cljs$core$IFn$_invoke$arity$3;
+} else
+{return and__3941__auto__;
+}
+})())
+{return this$.cljs$core$IFn$_invoke$arity$3(this$,a,b);
+} else
+{var x__3502__auto__ = (((this$ == null))?null:this$);return (function (){var or__3943__auto__ = (cljs.core._invoke[goog.typeOf(x__3502__auto__)]);if(or__3943__auto__)
+{return or__3943__auto__;
+} else
+{var or__3943__auto____$1 = (cljs.core._invoke["_"]);if(or__3943__auto____$1)
+{return or__3943__auto____$1;
+} else
+{throw cljs.core.missing_protocol.call(null,"IFn.-invoke",this$);
+}
+}
+})().call(null,this$,a,b);
+}
+});
+var _invoke__4 = (function (this$,a,b,c){if((function (){var and__3941__auto__ = this$;if(and__3941__auto__)
+{return this$.cljs$core$IFn$_invoke$arity$4;
+} else
+{return and__3941__auto__;
+}
+})())
+{return this$.cljs$core$IFn$_invoke$arity$4(this$,a,b,c);
+} else
+{var x__3502__auto__ = (((this$ == null))?null:this$);return (function (){var or__3943__auto__ = (cljs.core._invoke[goog.typeOf(x__3502__auto__)]);if(or__3943__auto__)
+{return or__3943__auto__;
+} else
+{var or__3943__auto____$1 = (cljs.core._invoke["_"]);if(or__3943__auto____$1)
+{return or__3943__auto____$1;
+} else
+{throw cljs.core.missing_protocol.call(null,"IFn.-invoke",this$);
+}
+}
+})().call(null,this$,a,b,c);
+}
+});
+var _invoke__5 = (function (this$,a,b,c,d){if((function (){var and__3941__auto__ = this$;if(and__3941__auto__)
+{return this$.cljs$core$IFn$_invoke$arity$5;
+} else
+{return and__3941__auto__;
+}
+})())
+{return this$.cljs$core$IFn$_invoke$arity$5(this$,a,b,c,d);
+} else
+{var x__3502__auto__ = (((this$ == null))?null:this$);return (function (){var or__3943__auto__ = (cljs.core._invoke[goog.typeOf(x__3502__auto__)]);if(or__3943__auto__)
+{return or__3943__auto__;
+} else
+{var or__3943__auto____$1 = (cljs.core._invoke["_"]);if(or__3943__auto____$1)
+{return or__3943__auto____$1;
+} else
+{throw cljs.core.missing_protocol.call(null,"IFn.-invoke",this$);
+}
+}
+})().call(null,this$,a,b,c,d);
+}
+});
