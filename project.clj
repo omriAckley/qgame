87bd6c370a72fac68358af8a81450df5c4f0acd6
Omri Bernstein <ob08@hampshire.edu>
2013-12-10 17:23:42 -0500

    Change to MIT license

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.hippiccolo/qgame "0.1.2"
   :description "Quantum Gate And Measurement Emulator, or qgame. An instruction-level quantum computing simulator. Ported from Lee Spector's QGAME (written in Common Lisp)."
-  :license {:name "Eclipse Public License (EPL)"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://http://en.wikipedia.org/wiki/MIT_License"}
   :url "https://github.com/omriBernstein/qgame"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.mikera/core.matrix "0.14.0"]
