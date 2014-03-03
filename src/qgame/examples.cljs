a22ab379b5e579e0fcfddb65318a4cce5aedf0f6
Omri Bernstein <hippiccolo@gmail.com>
2014-03-03 10:30:11 -0500

    Oracle gates have been tested and work. Adds example with an oracle.

--- src/qgame/examples.cljs
+++ src/qgame/examples.cljs
@@ -12,11 +12,12 @@
                      (u-theta 0.62832 0)
                      (cnot 0 1)
                      (hadamard 1)))
-#_(should output approx
-          ({:amplitudes [0.572 0.572 0.416 -0.416]
-            :prior-probability 1
-            :oracle-count 0
-            :measurement-history ()}))
+;should output approx
+;({:amplitudes [0.572 0.572 0.416 -0.416]
+;  :prior-probability 1
+;  :oracle-count 0
+;  :measurement-history ()})
+
 
 ;Example with complex numbers
 #_(execute-program {:num-qubits 2}
@@ -25,11 +26,23 @@
                      (u-theta 0.6283 0)
                      (cphase 0.1234 1 0)
                      (hadamard 1)))
-;(should output approx
-;          ({:amplitudes [0.988 #<0.156 + 0.01i> 0 #<5.948e-4 - 0.01i>],
-;            :prior-probability 1,
-;            :oracle-count 0,
-;            :measurement-history ()}))
+;should output approx
+;({:amplitudes [0.988 #<0.156 + 0.01i> 0 #<5.948e-4 - 0.01i>],
+;  :prior-probability 1,
+;  :oracle-count 0,
+;  :measurement-history ()})
+
+
+;Example with oracle
+#_(execute-program {:num-qubits 3
+                    :oracle [1 1 1 0]}
+                   '((oracle 2 1 0)))
+;should output approx
+;({:amplitudes [0 0 0 0 1 0 0 0],
+;  :prior-probability 1,
+;  :oracle-count 1,
+;  :measurement-history ()})
+
 
 ;Measurement example
 #_(execute-program {:num-qubits 3}
@@ -45,25 +58,26 @@
                      (end)
                      (end)
                      (end)))
-#_(should output approx
-          ({:amplitudes [0 0.5 0 0 0 0 0 0]
-            :prior-probability 0.25
-            :oracle-count 0
-            :measurement-history ([2 0] [0 1])}
-           {:amplitudes [0 0 0 0 0 0.5 0 0]
-            :prior-probability 0.25
-            :oracle-count 0
-            :measurement-history ([2 1] [0 1])}
-           {:amplitudes [0 0 0.5 0 0 0 0 0]
-            :prior-probability 0.25
-            :oracle-count 0
-            :measurement-history ([1 1] [0 0])}
-           {:amplitudes [0.5 0 0 0 0 0 0 0]
-            :prior-probability 0.25
-            :oracle-count 0
-            :measurement-history ([1 0] [0 0])}))
+;should output approx
+;({:amplitudes [0 0.5 0 0 0 0 0 0]
+;  :prior-probability 0.25
+;  :oracle-count 0
+;  :measurement-history ([2 0] [0 1])}
+; {:amplitudes [0 0 0 0 0 0.5 0 0]
+;  :prior-probability 0.25
+;  :oracle-count 0
+;  :measurement-history ([2 1] [0 1])}
+; {:amplitudes [0 0 0.5 0 0 0 0 0]
+;  :prior-probability 0.25
+;  :oracle-count 0
+;  :measurement-history ([1 1] [0 0])}
+; {:amplitudes [0.5 0 0 0 0 0 0 0]
+;  :prior-probability 0.25
+;  :oracle-count 0
+;  :measurement-history ([1 0] [0 0])})
 
-;;With custom step-by-step renderer
+
+;Now with custom step-by-step renderer
 #_(execute-program {:num-qubits 3
                     :renderer (fn [branches instruction]
                                 (println "head branch amplitudes...")
@@ -89,4 +103,4 @@
                      (end)
                      (end)
                      (end)))
-#_(should output same as above (but with intermediate printing))
+;should output same as above (but with intermediate printing)
